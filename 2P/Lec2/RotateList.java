a8c6a9cd9151c81cdc43dda2e1ff510c0c96fa08
Azhar Beg <azharsubhan10@gmail.com>
2025-03-05 13:27:23 +0530

    doubly LL class

---
+++ 2P/Lec2/RotateList.java
@@ -0,0 +1,38 @@
+package 2P.Lec3;
+
+public class RotateList {
+
+    public ListNode findNthNode(ListNode temp, int k){
+        int cnt = 1;
+        while(temp != null){
+            if(cnt == k) return temp;
+            cnt++;
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        
+        if(head == null || head.next == null) return head;
+        ListNode tail = head;
+        int length = 1;
+        
+        while(tail.next != null){
+            tail = tail.next;
+            length++;
+        }
+        // length and tail found
+        if(k % length == 0) return head; 
+        k = k % length;
+       
+        tail.next = head;  // 1 step
+
+        ListNode newLastNode = findNthNode(head, length - k);
+
+        head = newLastNode.next;
+        newLastNode.next = null;       
+
+        return head;   
+    }
+}
