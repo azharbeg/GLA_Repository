3b05430a39601bd1c3a6dd0d2fed018dc83452c1
Azhar Beg <azharsubhan10@gmail.com>
2025-03-26 21:52:17 +0530

    Section - H4

---
+++ 2H/Lec4/SlidingWindowMaximum.java
@@ -0,0 +1,38 @@
+package 2H.Lec4;
+
+public class SlidingWindowMaximum {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+        // // for the first window
+        int n = nums.length;
+        if(n == 0 || k == 0) return new int[0];
+        int maxi = Integer.MIN_VALUE;
+        int[] arr = new int[n-k+1] ; // number of window
+
+        for(int i = 0; i < k; i++){
+
+          maxi = Math.max(maxi,nums[i]);
+        }
+            
+        arr[0] = maxi;
+        int start = 1;
+        int end = k;
+        
+        while(end < n){
+            maxi = Integer.MIN_VALUE;
+            for(int i = start; i <= end; i++){
+
+                    maxi = Math.max(maxi, nums[i]);
+            }
+
+            // yha maxi mai maximum element hai usko array mai daal denge;
+            arr[start] = maxi;
+
+            // start or end ko increase kr do
+            start++;
+            end++;
+        }
+        return arr;
+    }
+}
