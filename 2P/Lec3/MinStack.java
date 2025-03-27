af9276cd01a3e5b01e6ef0713418d741cabf1182
Azhar Beg <azharsubhan10@gmail.com>
2025-03-27 15:33:28 +0530

    Section - P3

---
+++ 2P/Lec3/MinStack.java
@@ -0,0 +1,32 @@
+package 2P.Lec3;
+
+public class MinStack {
+
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stackobj = new Stack<Integer>();
+
+    public MinStack() {
+       // Stack<Integer> stackobj = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        
+        if(val <= min){
+            stackobj.push(min);
+            min = val;
+        }
+        stackobj.push(val);
+    }
+    
+    public void pop() {
+        if(stackobj.pop() == min) min = stackobj.pop();
+    }
+    
+    public int top() {
+        return stackobj.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
