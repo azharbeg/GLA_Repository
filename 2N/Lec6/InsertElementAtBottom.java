dd802d2baecfc9ddeef2ec1d45b06d2f5b3f304d
Azhar Beg <azharsubhan10@gmail.com>
2025-03-27 16:42:14 +0530

    section - N6

---
+++ 2N/Lec6/InsertElementAtBottom.java
@@ -0,0 +1,34 @@
+package 2N.Lec6;
+
+public class InsertElementAtBottom {
+
+    public static void insertAtBottom(Stack<Integer> stack, int x) {
+        // Base case: If the stack is empty, push the element
+        if (stack.isEmpty()) {
+            stack.push(x);
+            return;
+        }
+
+        // Remove the top element and store it
+        int temp = stack.pop();
+
+        // Recursive call to insert the element at bottom
+        insertAtBottom(stack, x);
+
+        // Push the stored element back to the stack
+        stack.push(temp);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        
+        System.out.println("Original Stack: " + stack);
+        
+        insertAtBottom(stack, 5);
+
+        System.out.println("Stack After Inserting 5 at Bottom: " + stack);
+    }
+}
