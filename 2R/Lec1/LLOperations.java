e7c07b7fc338b803adfc22beb3ab15c811bc5e7b
Azhar Beg <azharsubhan10@gmail.com>
2025-03-27 14:52:40 +0530

    R1

--- 2R/Lec1/LLOperations.java
+++ 2R/Lec1/LLOperations.java
@@ -1,3 +1,172 @@
+import java.util.LinkedList;
+
 public class LLOperations {
 
+      
+    Node head;
+    // Node structure (Thinks, You should know)
+      class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Insert at the beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert at the end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+//     // Insert at a specific position (1-based index)
+    public void insertAtPosition(int data, int position) {
+        if (position < 1) {
+            System.out.println("Invalid position!");
+            return;
+        }
+        Node newNode = new Node(data);
+        if (position == 1) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; temp != null && i < position - 1; i++) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Position out of bounds");
+            return;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // Delete from the beginning
+    public void deleteFromBeginning() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        head = head.next;
+    }
+
+//     // Delete from the end
+    public void deleteFromEnd() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    // Delete from a specific position (1-based index)
+    public void deleteFromPosition(int position) {
+        if (head == null || position < 1) {
+            System.out.println("Invalid position or list is empty");
+            return;
+        }
+        if (position == 1) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; temp != null && i < position - 1; i++) {
+            temp = temp.next;
+        }
+        if (temp == null || temp.next == null) {
+            System.out.println("Position out of bounds");
+            return;
+        }
+        temp.next = temp.next.next;
+    }
+
+    // Search for a value
+    public boolean search(int key) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    // Get the size of the linked list
+    public int getSize() {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    // Display the linked list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    
+     
+
+        public static void main(String[] args) {
+            LinkedList list = new LinkedList();
+            list.insertAtEnd(10);
+            list.insertAtEnd(20);
+            list.insertAtEnd(30);
+            list.display();
+    
+            list.insertAtBeginning(5);
+            list.display();
+    
+            list.insertAtPosition(15, 3);
+            list.display();
+    
+            list.deleteFromBeginning();
+            list.display();
+    
+            list.deleteFromEnd();
+            list.display();
+    
+            list.deleteFromPosition(2);
+            list.display();
+    
+            System.out.println("Search 20: " + list.search(20));
+            System.out.println("Search 100: " + list.search(100));
+    
+            System.out.println("Size of the list: " + list.getSize());
+        }
 }
