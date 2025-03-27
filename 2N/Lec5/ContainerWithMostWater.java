430ed6e804828c619d1820326198bc88a749b6cc
Azhar Beg <azharsubhan10@gmail.com>
2025-03-27 16:34:36 +0530

    Section-N5

---
+++ 2N/Lec5/ContainerWithMostWater.java
@@ -0,0 +1,30 @@
+package 2N.Lec5;
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        
+        int n = height.length;
+        int i = 0;
+        int j = n - 1;
+        int ans =0;
+
+        while(i < j){
+
+           ans = Math.max(ans,(j-i) * Math.min(height[i],height[j]));
+           
+           if(height[i] < height[j]){
+               i++;
+           }
+           else if(height[i] > height[j]){
+               j--;
+           }
+           // if heights are equal
+           else{
+               i++;
+               j--;
+           }
+        }
+       return ans;
+   }
+}
