a38d50661fb81f817b0e391c884369f0cf8eec2e
Azhar Beg <azharsubhan10@gmail.com>
2025-03-27 14:36:39 +0530

    Section- R3

---
+++ 2R/Lec3/ValidParentheses.java
@@ -0,0 +1,27 @@
+package 2R.Lec3;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s)
+    {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0, n=s.length(); i<n; i++)
+        {
+            char c=s.charAt(i);
+            if(st.isEmpty())
+            {
+                if(c==')'||c=='}'||c==']') return false;
+                else st.push(c);
+            }
+            else
+            {
+                if(c==')' && st.peek()=='(') st.pop();
+                else if(c=='}' && st.peek()=='{') st.pop();
+                else if(c==']' && st.peek()=='[') st.pop();
+                else st.push(c);
+            }
+        }
+        return st.isEmpty();
+    }
+}
