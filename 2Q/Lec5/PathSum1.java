af2cc6d152cfc4514ad51afe7e354e7f5fba20f5
Azhar Beg <azharsubhan10@gmail.com>
2025-04-15 09:56:44 +0530

    Tree Question

---
+++ 2Q/Lec5/PathSum1.java
@@ -0,0 +1,16 @@
+
+public class PathSum1 {
+
+   
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        
+        if(root == null)
+             return false;
+
+         if(root.left == null && root.right == null)
+              return root.val == targetSum;
+
+         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+}
