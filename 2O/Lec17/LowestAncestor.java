dc37f4f839105e150f13b97a14cf109f01fa92a9
Azhar Beg <azharsubhan10@gmail.com>
2025-04-17 15:34:28 +0530

    BST

--- 2O/Lec17/LowestAncestor.java
+++ 2O/Lec17/LowestAncestor.java
@@ -1,5 +1,28 @@
-package 2O.Lec17;
-
-public class LowestAncestor {
-
+ 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class LowestAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+          if(root == null || root == p || root == q){
+            return root;
+        }
+        TreeNode leftlca = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightlca = lowestCommonAncestor(root.right,p,q);
+        
+        if(rightlca == null){
+            return leftlca;
+        }
+        if(leftlca == null){
+            return rightlca;
+        }
+        return root;
+    }
 }
