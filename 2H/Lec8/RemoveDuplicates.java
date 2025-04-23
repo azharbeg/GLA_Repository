6e9f9cc5a8dbcf26c69e926f91ca7827b58c219d
Azhar Beg <azharsubhan10@gmail.com>
2025-04-23 11:08:28 +0530

    Leetcode Practice

---
+++ 2H/Lec8/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package 2H.Lec8;
+
+public class RemoveDuplicates {
+
+    int removeDuplicates(vector<int>& nums) {
+        
+
+        int n = nums.size();
+        int k = 0;
+        int i;
+
+        for( i = 0; i < n -1; i++)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                continue;
+            }
+            else{
+                nums[k++] = nums[i];   // 0,1,2,3
+            }
+        }
+        // how to handle last element 4,4 or 4
+
+        nums[k] = nums[i];
+
+        int size = k + 1;
+
+        return size;
+    }
+}
